1627edf9c53116433a58e3420f3003560eaba63c
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2024-07-24 14:59:16 +0900

    (edit) Basic_command

--- testbot.py
+++ testbot.py
@@ -9,8 +9,8 @@ CHANNEL_ID = os.getenv('CHANNEL_ID')
 
 # Define intents
 intents = discord.Intents.default()
-# intents.guild_voice_states = True  # 음성 상태 변경 인텐트 활성화
 # Initialize bot with intents
+# 명령어 시작부분
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
@@ -20,11 +20,6 @@ async def on_ready():
 @bot.command(name='hello')
 async def hello(ctx):
     await ctx.send('안녕하세요')
-
-# 따라하기
-@bot.command(name='!') # !!
-async def hello(ctx, text:str=None):
-    await ctx.send(text)
 
 # 봇 작동
 bot.run(TOKEN)
