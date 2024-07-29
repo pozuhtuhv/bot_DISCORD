0083e612451b49944305a0efa38b51c6cdc0304a
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2024-07-29 14:55:34 +0900

    (edit) value_set

--- testbot.py
+++ testbot.py
@@ -5,7 +5,7 @@ from navertts import NaverTTS
 from dotenv import load_dotenv
 import time
 import sys
-from function import ymusic
+from function.ymusic import *
 
 # .env 파일 활성화
 load_dotenv()
@@ -15,6 +15,12 @@ VOICE_CHANNEL_ID = os.getenv('VOICE_CHANNEL_ID')
 VOICE_TEXTCHANNEL_ID = os.getenv('VOICE_TEXTCHANNEL_ID')
 ADMIN_ID = os.getenv('ADMIN_ID')
 STAFF_ROLE = "Admin"
+
+# Youtube Stream 
+progress_message = None  # 이전 메시지를 저장
+start_time = None  # 노래 시작 시점
+current_task = None # 진행중인 노래
+current_video_url = None # 비디오 영상 주소
 
 # Define intents
 intents = discord.Intents.default()
@@ -35,7 +41,6 @@ async def on_ready():
 # 봇 재시작
 @bot.command(name='restart')
 async def restart(ctx):
-    """봇을 재시작합니다."""
     try:
         if any(role.name == str(STAFF_ROLE) for role in ctx.author.roles):
             await ctx.send("봇을 재시작합니다.")
@@ -54,11 +59,8 @@ async def restart(ctx):
 # ROLE 확인
 @bot.command(name='myrole')
 async def list_roles(ctx):
-    """사용자의 역할을 출력합니다."""
     # 사용자의 역할 목록 가져오기
     roles = ctx.author.roles
-    
-    # 역할 이름만 추출 (역할 리스트의 첫 번째는 항상 @everyone이므로 제외)
     role_names = [role.name for role in roles if role.name != "@everyone"]
 
     if role_names:
@@ -66,95 +68,6 @@ async def list_roles(ctx):
         await ctx.send(f"{ctx.author.mention}님의 역할: " + ", ".join(role_names))
     else:
         await ctx.send(f"{ctx.author.mention}님은 특별한 역할이 없습니다.")
-
-# test
-progress_message = None  # 이전 메시지를 저장
-start_time = None  # 노래 시작 시점
-current_task = None # 진행중인 노래
-current_video_url = None # 비디오 영상 주소
-
-# 음성 채널에 연결하여 YouTube 음악 재생
-@bot.command(name='ymusic')
-async def play(ctx, url):
-    global start_time
-    try:
-        channel = bot.get_channel(int(VOICE_CHANNEL_ID))
-
-        # 음성 클라이언트 생성 및 음성 채널로 이동
-        if ctx.voice_client is None:
-            await channel.connect()
-        else:
-            await ctx.voice_client.move_to(channel)
-
-        # YouTube URL에서 오디오 스트리밍 소스 가져오기
-        audio_url, title, duration, original_url = ymusic.get_youtube_audio_source(url)
-
-        # source = discord.FFmpegPCMAudio(audio_url, options='-vn -bufsize 64M -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5')
-
-        source = discord.FFmpegPCMAudio(
-            audio_url,
-            before_options='-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-            options='-vn -bufsize 64M'
-        )
-
-        # 오디오 재생
-        start_time = time.time()
-        ctx.voice_client.play(source, after=lambda e: print(f'오류 발생: {e}') if e else None)
-
-        await ctx.send(f'<{original_url}> :notes: 재생중')
-
-        # 재생바 업데이트를 위한 루프 시작
-        # 이전 메시지 초기화
-        progress_message = None
-        start_time = time.time()  # 시작 시간 갱신
-        update_progress_bar.start(ctx, title, duration)
-
-    except Exception as e:
-        await ctx.send(f"오류가 발생했습니다: {str(e)}")
-
-@tasks.loop(seconds=2)
-async def update_progress_bar(ctx, title, duration):
-    """2초마다 재생바 업데이트"""
-    global progress_message, start_time
-    if ctx.voice_client and ctx.voice_client.is_playing():
-        elapsed = time.time() - start_time  # 경과 시간 계산
-        progress_bar = ymusic.create_progress_bar(elapsed, duration)
-
-        try:
-            # 이전 메시지 업데이트
-            if progress_message is None:
-                progress_message = await ctx.send(f"{progress_bar} \n `{title}`")
-            else:
-                await progress_message.edit(content=f"{progress_bar} \n `{title}`")
-        except discord.errors.NotFound:
-            # 메시지가 삭제되었을 경우 새로운 메시지 보내기
-            progress_message = await ctx.send(f"{progress_bar} \n `{title}`")
-        except Exception as e:
-            print(f"메시지 업데이트 중 오류 발생: {e}")
-
-# 재생 중지 명령어
-@bot.command(name='stop')
-async def stop(ctx):
-    global current_task
-    
-    try:
-        # 음성 채널에 연결되어 있는지 확인
-        if ctx.voice_client is not None:
-            # 현재 진행 중인 Task가 있는 경우 취소
-            if current_task is not None and not current_task.done():
-                current_task.cancel()
-                await ctx.send("현재 재생 중인 음악을 중지하고 음성 채널에서 나갑니다.")
-
-            # 음성 채널 연결 해제
-            await ctx.voice_client.disconnect()
-            
-            # Task를 None으로 초기화
-            current_task = None
-            await ctx.send("재생을 중지하고 음성 채널에서 나갔습니다.")
-        else:
-            await ctx.send("봇이 음성 채널에 연결되어 있지 않습니다.")
-    except Exception as e:
-        await ctx.send(f"오류가 발생했습니다: {str(e)}")
 
 # 인사
 @bot.command(name='hello')
@@ -182,7 +95,7 @@ async def voiceout(ctx):
         await ctx.channel.purge(limit=99)
         await channel.disconnect()
 
-# tts test
+# Naver tts 출력
 @bot.command(name='tts')
 async def on_message(ctx, *, text:str):
     if str(ctx.channel.id) == str(VOICE_TEXTCHANNEL_ID):   
@@ -190,6 +103,94 @@ async def on_message(ctx, *, text:str):
         tts = NaverTTS(text, speed=0)
         tts.save('tts.mp3')
         voice.play(discord.FFmpegPCMAudio('tts.mp3'))
+
+# 음성 채널에 연결하여 YouTube 음악 재생
+@bot.command(name='ymusic')
+async def play(ctx, url):
+    global start_time, progress_message, current_task
+    try:
+        channel = bot.get_channel(int(VOICE_CHANNEL_ID))
+
+        # 음성 클라이언트 생성 및 음성 채널로 이동
+        if ctx.voice_client is None:
+            await channel.connect()
+        else:
+            await ctx.voice_client.move_to(channel)
+
+        # 이전 음악의 진행바 업데이트 중지
+        if current_task is not None and not current_task.done():
+            current_task.cancel()
+
+        # YouTube URL에서 오디오 스트리밍 소스 가져오기
+        audio_url, title, duration, original_url = get_youtube_audio_source(url)
+
+        source = discord.FFmpegPCMAudio(
+            audio_url,
+            before_options='-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+            options='-vn -bufsize 64M'
+        )
+
+        # 오디오 재생
+        start_time = time.time()
+        ctx.voice_client.play(source, after=lambda e: print(f'오류 발생: {e}') if e else None)
+
+        await ctx.send(f'<{original_url}> :notes: 재생중')
+
+        # 진행바 초기화
+        progress_message = None
+        start_time = time.time()  # 시작 시간 갱신
+
+        # 새롭게 태스크 시작
+        current_task = update_progress_bar.start(ctx, title, duration)
+
+    except Exception as e:
+        await ctx.send(f"오류가 발생했습니다: {str(e)}")
+
+@tasks.loop(seconds=2)
+async def update_progress_bar(ctx, title, duration):
+    """2초마다 재생바 업데이트"""
+    global progress_message, start_time
+    if ctx.voice_client and ctx.voice_client.is_playing():
+        elapsed = time.time() - start_time  # 경과 시간 계산
+        progress_bar = create_progress_bar(elapsed, duration)
+
+        try:
+            # 이전 메시지 업데이트
+            if progress_message is None:
+                progress_message = await ctx.send(f"{progress_bar} \n `{title}`")
+            else:
+                await progress_message.edit(content=f"{progress_bar} \n `{title}`")
+        except discord.errors.NotFound:
+            # 메시지가 삭제되었을 경우 새로운 메시지 보내기
+            progress_message = await ctx.send(f"{progress_bar} \n `{title}`")
+        except Exception as e:
+            print(f"메시지 업데이트 중 오류 발생: {e}")
+
+# 재생 중지 명령어
+@bot.command(name='stop')
+async def stop(ctx):
+    global current_task, progress_message, start_time
+    
+    try:
+        # 음성 채널에 연결되어 있는지 확인
+        if ctx.voice_client is not None:
+            # 현재 진행 중인 Task가 있는 경우 취소
+            if current_task is not None and not current_task.done():
+                current_task.cancel()
+
+            # 오디오 재생 중지
+            ctx.voice_client.stop()
+
+            # 진행바 관련 변수 초기화
+            current_task = None
+            progress_message = None
+            start_time = None
+
+            await ctx.send("현재 재생 중인 음악을 중지합니다.")
+        else:
+            await ctx.send("봇이 음성 채널에 연결되어 있지 않습니다.")
+    except Exception as e:
+        await ctx.send(f"오류가 발생했습니다: {str(e)}")
 
 # 봇 작동
 bot.run(TOKEN)
